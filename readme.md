52ed0fe756c3af1da08a967a647b24f4ae3fc2e7
tycm <44624889+tycm@users.noreply.github.com>
2021-03-03 10:56:44 -0500

    updated documentation for selector.py

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Scripts
+
+This is jsut a repository or random scripts that I wrote when I was either bored, or had a need for a simple script.
--- selector.py
+++ selector.py
@@ -1,3 +1,15 @@
+# This script expects a text document titled 'employees.txt' to work.
+# It should be formatted as follows
+# The first line is the number of employees that you would like to select. Please keep this as a number with no extra characters on this line.
+# Every subsequent line is an employee, please only use letters, as I am not sure how it will react to different characters.
+"""
+2
+Employee One
+Employee Two
+Employee Three
+"""
+
+
 import random
 
 f = (open('employees.txt'))
