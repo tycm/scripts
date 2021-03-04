e46bc6c60c7b2c74b6e3eda2125e124ab757bcbf
tycm <44624889+tycm@users.noreply.github.com>
2021-03-04 09:26:51 -0500

    Changed open to use with syntax

--- selector.py
+++ selector.py
@@ -12,14 +12,14 @@ Employee Three
 
 import random
 
-f = (open('employees.txt'))
-hold = f.read().splitlines()
-employees = hold[1:]
-numEmployees = int(hold[0])
+with open('employees.txt') as f:
+    hold = f.read().splitlines()
+    employees = hold[1:]
+    numEmployees = int(hold[0])
 
-if numEmployees > (len(employees)):
-    print(f"Trying to choose {numEmployees} when only {len(employees)} to choose from")
-else:
-    print(random.sample(employees,numEmployees))
+    if numEmployees > (len(employees)):
+        print(f"Trying to choose {numEmployees} when only {len(employees)} to choose from")
+    else:
+        print(random.sample(employees,numEmployees))
 
-input("Press enter to close")
+    input("Press enter to close")
